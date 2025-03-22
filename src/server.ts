6d4eba2fd49a04776cbce96c587f84d74f44439f
402882 <402882@superai.aiat>
2025-03-22 19:15:15 +0700

    [UPDATE] PORT 8080

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import routes from "./routes/routes";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
